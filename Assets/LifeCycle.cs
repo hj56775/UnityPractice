12a37c89e5ccc5c7f8467ae1153509e9e091d4ab
hj56775 <68840193+hj56775@users.noreply.github.com>
2021-01-28 17:52:38 +0900

    입력 학습

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LifeCycle : MonoBehaviour
 {
-    void Awake()//게임 오브젝트 생성할 때 최초 실행
+    /*void Awake()//게임 오브젝트 생성할 때 최초 실행
     {
         Debug.Log("플레이어 데이터가 준비되었습니다.");
     }
@@ -39,5 +39,33 @@ public class LifeCycle : MonoBehaviour
     void OnDestroy()//오브젝트가 삭제될 때
     {
         Debug.Log("플레이어 데이터를 해제하였습니다.");
+    }*/
+    void Update()
+    {
+        if (Input.anyKeyDown)//아무 입력을 최로로 받을 때 true
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+        /*키를 계속 누르고 있으면 true
+           if (Input.anyKey)
+               Debug.Log("플레이어가 아무 키를 누르고 있습니다.");*/
+        if (Input.GetKeyDown(KeyCode.Return))//Enter=Return, ESC=Escape
+            Debug.Log("아이템을 구입하였습니다.");
+        if (Input.GetKey(KeyCode.LeftArrow))
+            Debug.Log("왼쪽으로 이동 중");
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            Debug.Log("오른쪽 이동을 멈추었습니다.");
+
+        //마우스 입력(0==왼쪽,1==오른쪽)
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("미사일 발사!");
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("미사일 모으는 중...");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("슈퍼 미사일 발사!!");
+        }
     }
 }
